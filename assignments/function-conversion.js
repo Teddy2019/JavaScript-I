4149aeb9ee4688ac8800ba3f3330b66594ab4090
Your Name <tfikrie@yahoo.com>
2019-04-17 20:19:48 -0400

    objects completed

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -28,8 +28,8 @@
 
 // Stretch
 
-// exampleArray = [1,2,3,4];
-// const triple = exampleArray.map(function (num) {
-//   return num * 3;
-// });
-// console.log(triple);
+ exampleArray = [1,2,3,4];
+ const triple = exampleArray.map((num) => {
+  return num * 3;
+ });
+ console.log(triple);
